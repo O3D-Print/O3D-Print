857ad5c6f1d2c6dcf460f754e1801ea1ce71bf00
O3D Print <o3d.print.o@gmail.com>
2026-04-10 16:39:10 +0200

    V1.6.1

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const productos = [
 
 let carrito = [];
 
-// Cargar catálogo al inicio
 window.onload = () => {
     const grid = document.getElementById('product-grid');
     if(grid) {
@@ -68,71 +67,15 @@ function toggleCart() {
     document.getElementById('cart-sidebar').classList.toggle('active');
 }
 
-// FLUJO DE PAGO CON KO-FI
 function procesarPago() {
     if (carrito.length === 0) return alert("La cesta está vacía");
     
-    // 1. Calculamos el total y los nombres de los productos
     const total = carrito.reduce((sum, p) => sum + p.precio, 0);
     const articulos = carrito.map(i => i.nombre).join(", ");
 
-    // 2. Guardamos los datos en localStorage (memoria del navegador)
     localStorage.setItem('totalO3D', total);
     localStorage.setItem('articulosO3D', articulos);
     
-    // 3. Redirigimos a tu nueva página de checkout
-    window.location.href = 'pago.html';
-}
-function mostrarFormularioEnvio() {
-    document.getElementById('form-modal').style.display = 'flex';
-}
-
-function cerrarModal() {
-    document.getElementById('form-modal').style.display = 'none';
-    // Volvemos a mostrar el botón de pago por si quiere corregir algo
-    document.getElementById('btn-pagar-main').style.display = 'block';
-    document.getElementById('confirmacion-pago').style.display = 'none';
-}
-
-const SCRIPT_URL_GOOGLE = "https://script.google.com/macros/s/AKfycbyulcHYSz_OD3GRKXU7RCZkxFm9pY5Xi_afdZJW_BoyEI0hK0WaWDYV3Sy1We4Sq9tf/exec";
-
-async function enviarPedido() {
-    const nombre = document.getElementById('u-nombre').value;
-    const email = document.getElementById('u-email').value;
-    const idpago = document.getElementById('u-id-pago').value;
-    const notas = document.getElementById('u-notas').value;
-
-    if (!nombre || !email || !idpago) {
-        return alert("Por favor, rellena los campos obligatorios para el envío.");
-    }
-
-    const datos = {
-        nombre: nombre,
-        email: email,
-        idpago: idpago,
-        notas: notas,
-        total: document.getElementById('total-price').innerText,
-        articulos: carrito.map(i => i.nombre).join(", ")
-    };
-
-    alert(`¡Gracias ${nombre}! Enviando datos a O3D-Print...`);
-    
-    // Limpieza
-    carrito = [];
-    actualizarCarrito();
-    cerrarModal();
-    if (document.getElementById('cart-sidebar').classList.contains('active')) {
-        toggleCart();
-    }
-    
-    // Enviar a Google de forma silenciosa
-    try {
-        fetch(SCRIPT_URL_GOOGLE, {
-            method: 'POST',
-            mode: 'no-cors', 
-            body: JSON.stringify(datos)
-        });
-    } catch (error) {
-        console.log("Error de red");
-    }
+    // Redirección limpia a la carpeta pago
+    window.location.href = './pago/';
 }
